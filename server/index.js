31d9565b985ef669cd5c35e9cb182f3663a38c27
Samandar Qodirqulov <samandarqodirqulov021@gmail.com>
2022-05-10 23:03:10 +0500

    a

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const fs = require("fs");
 app.use(cors);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(morgan("common"));
 
 app.use((req, res, next) => {
   res.setHeader("Acces-Control-Allow-Origin", "*");
